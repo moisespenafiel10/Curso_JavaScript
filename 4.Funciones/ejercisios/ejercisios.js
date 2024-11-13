e6de6c9b7afdbd842a1bd384e6ba45ee2ca9eaed
moisespenafiel10 <penafieltellomoi@gmail.com>
2024-11-12 22:01:21 -0500

    feat: Ejercisio funciones
    Terminado

--- 4.Funciones/ejercisios/ejercisios.js
+++ 4.Funciones/ejercisios/ejercisios.js
@@ -13,21 +13,17 @@
 // 2. haciendo uso de la recursion crear una funcion que me muestre los primeros 20 numeros de la serie fibonacci:
 
 function fibonacci(n) {
-
     if (n <= 1) {
         return n;
     }
-
 
     return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 function mostrarFibonacci(cantidad) {
-
     for (let i = 0; i < cantidad; i++) {
         console.log(fibonacci(i));
     }
 }
-
 
 mostrarFibonacci(5);  
